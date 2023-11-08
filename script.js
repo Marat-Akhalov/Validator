bf2c30157db58e253a42afae0e9d3e1133d0085b
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-08 19:02:28 +0400

    add: add email validation func

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const userName = document.querySelector('.form__input--user-name');
 const email = document.querySelector('.form__input--email');
 const password = document.querySelector('.form__input--password');
 const password2 = document.querySelector('.form__input--password-2');
+
+const isEmailValid = email => {
+  const regExpPattern =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  return regExpPattern.test(email);
+};
