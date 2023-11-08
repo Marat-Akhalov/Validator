e4894f4144d5934f7d5a6f41403a6dd33aab0969
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-08 18:57:15 +0400

    add: get and add inputs variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('Hello from js');
+const form = document.querySelector('.form');
+const userName = document.querySelector('.form__input--user-name');
+const email = document.querySelector('.form__input--email');
+const password = document.querySelector('.form__input--password');
+const password2 = document.querySelector('.form__input--password-2');
