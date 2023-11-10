21a04327b0d1d1aa915edbcfc9b66a79cbf8edee
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-10 19:10:20 +0400

    add: add checkEmail and checkPassword funcs to event listener

--- script.js
+++ script.js
@@ -67,4 +67,6 @@ form.addEventListener('submit', e => {
   checkRequired([userName, email, password, password2]);
   checkLength(userName, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
